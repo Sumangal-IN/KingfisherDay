473d5098595f284039761a364878e12fe42334a5
Sumangal <iamsumangal@gmail.com>
2018-09-05 23:15:53 +0530

    Adding flag repository

---
+++ KingfisherDay/src/main/java/com/tcs/KingfisherDay/controller/FlagController.java
@@ -0,0 +1,32 @@
+package com.tcs.KingfisherDay.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.tcs.KingfisherDay.model.Flag;
+import com.tcs.KingfisherDay.service.FlagService;
+
+@Controller
+public class FlagController {
+
+	@Autowired
+	FlagService flagService;
+
+	@RequestMapping(value = "/getAllFlags", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public List<Flag> getAllFlags() {
+		return flagService.getAllFlags();
+	}
+
+	@RequestMapping(value = "/updateFlag/{key}/{value}", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public Flag updateFlag(@PathVariable("key") String key, @PathVariable("value") String value) {
+		return flagService.updateFlag(key, Boolean.valueOf(value));
+	}
+
+}
--- KingfisherDay/src/main/resources/data.sql
+++ KingfisherDay/src/main/resources/data.sql
@@ -3,3 +3,6 @@ insert into question(question_ID,question_Text,option_A,option_B,option_C,option
 insert into question(question_ID,question_Text,option_A,option_B,option_C,option_D,option_Correct,current) values('3','question 3','option A','option B','option C','option D','B',false);
 insert into question(question_ID,question_Text,option_A,option_B,option_C,option_D,option_Correct,current) values('4','question 4','option A','option B','option C','option D','A',false);
 insert into question(question_ID,question_Text,option_A,option_B,option_C,option_D,option_Correct,current) values('5','question 5','option A','option B','option C','option D','B',false);
+
+insert into flag(key,value) values('quiz.end','false');
+insert into flag(key,value) values('app.active','true');
