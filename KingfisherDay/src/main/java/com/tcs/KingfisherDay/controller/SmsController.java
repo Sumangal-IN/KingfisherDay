5f0d678b270a80efc818b38acb6f07aad6ff4739
mandal_s <mandal_s@GBKTNEO021X0011.kits.kfplc.com>
2018-09-07 13:58:43 +0530

    Adding swagger & KITS proxy

--- KingfisherDay/src/main/java/com/tcs/KingfisherDay/controller/SmsController.java
+++ KingfisherDay/src/main/java/com/tcs/KingfisherDay/controller/SmsController.java
@@ -1,6 +1,11 @@
 package com.tcs.KingfisherDay.controller;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +19,18 @@ public class SmsController {
 	@RequestMapping(value = "/sendOTP/{mobile}/{otp}", method = RequestMethod.GET, produces = "application/text")
 	@ResponseBody
 	public String updateFlag(@PathVariable("mobile") String mobile, @PathVariable("otp") String otp) {
-		RestTemplate restTemplate=new RestTemplate();
-		ResponseEntity<String> result = restTemplate.getForEntity("http://api.msg91.com/api/sendhttp.php?authkey=130190AmlXD2ELBEmi581b5034&mobiles=91"
-				+ mobile
-				+ "&message="
-				+ otp
-				+ "&sender=KFSDAY&route=4&country=91", String.class);
+
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+
+		Proxy proxy = new Proxy(Type.HTTP, new InetSocketAddress("webproxy.ext.gha.kfplc.com", 8080));
+		requestFactory.setProxy(proxy);
+
+		RestTemplate restTemplate = new RestTemplate();
+		System.out.println("http://api.msg91.com/api/sendhttp.php?authkey=130190AmlXD2ELBEmi581b5034&mobiles=91"
+				+ mobile + "&message=" + otp + "&sender=KFSDAY&route=4&country=91");
+		ResponseEntity<String> result = restTemplate
+				.getForEntity("http://api.msg91.com/api/sendhttp.php?authkey=130190AmlXD2ELBEmi581b5034&mobiles=91"
+						+ mobile + "&message=" + otp + "&sender=KFSDAY&route=4&country=91", String.class);
 		return result.getBody();
 	}
 
