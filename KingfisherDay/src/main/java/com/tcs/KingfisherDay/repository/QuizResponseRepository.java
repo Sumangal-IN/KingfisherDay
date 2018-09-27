b59833a1031abba08aa9ecbaa75164cf0cba15c9
Tuhin Chandra <santuc1990@gmail.com>
2018-09-27 10:31:06 +0100

    Corrected Qury

--- KingfisherDay/src/main/java/com/tcs/KingfisherDay/repository/QuizResponseRepository.java
+++ KingfisherDay/src/main/java/com/tcs/KingfisherDay/repository/QuizResponseRepository.java
@@ -10,7 +10,7 @@ import com.tcs.KingfisherDay.model.QuizResponse;
 @Repository
 public interface QuizResponseRepository extends JpaRepository<QuizResponse, String> {
 
-	@Query("select q from QuizResponse q where q.questionID=:questionID and instr(:optionCorrect,q.option)>0 order by q.timeStamp desc limit 1")
+	@Query("select q from QuizResponse q where q.questionID=:questionID and instr(:optionCorrect,q.option)>0 order by q.timeStamp desc")
 	QuizResponse findTopByQuestionIDAndOptionOrderByTimeStamp(@Param("questionID") String questionID, @Param("optionCorrect") String optionCorrect);
 
 	long countByOptionAndQuestionID(String string, String questionID);
